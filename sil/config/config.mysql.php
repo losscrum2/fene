020358ef396e7b51e149c029108196271a566d4d
ariel <ariel_gavilanes1@hotmail.com>
2017-11-22 21:48:02 -0500

    Correcion sil

---
+++ sil/config/config.mysql.php
@@ -0,0 +1,13 @@
+<?php
+	
+	// CONEXION A LA BASE DE DATOS MYSQL - KOICA
+	define( "MYSQL_HOST", "localhost" );
+	define( "MYSQL_BASEDATOS", "fenedif123" );
+	define( "MYSQL_USUARIO", "root" );
+	define( "MYSQL_CLAVE", "123456" ); 
+	//define( "MYSQL_HOST", "localhost" );
+	//define( "MYSQL_BASEDATOS", "fenedif2" );
+	//define( "MYSQL_USUARIO", "root" );
+	//define( "MYSQL_CLAVE", "root" ); 
+
+?>
